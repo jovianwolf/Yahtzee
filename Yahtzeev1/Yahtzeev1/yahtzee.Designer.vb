d91f330f10c55b975b604cdbf1f4880898e9f766
Patrick Reynolds <Made a change to Roll button to close the program as test>
2020-12-04 10:05:38 -0800

    Added Images folder to solution in Bin containing the Dice images, also removed the AI player section for now while we work on getting base game logic implemented

--- Yahtzeev1/Yahtzeev1/yahtzee.Designer.vb
+++ Yahtzeev1/Yahtzeev1/yahtzee.Designer.vb
@@ -33,17 +33,6 @@ Partial Class yahtzee
         Me.Button8 = New System.Windows.Forms.Button()
         Me.Button9 = New System.Windows.Forms.Button()
         Me.Button10 = New System.Windows.Forms.Button()
-        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
-        Me.Button11 = New System.Windows.Forms.Button()
-        Me.Button12 = New System.Windows.Forms.Button()
-        Me.Button13 = New System.Windows.Forms.Button()
-        Me.Button14 = New System.Windows.Forms.Button()
-        Me.Button15 = New System.Windows.Forms.Button()
-        Me.Button16 = New System.Windows.Forms.Button()
-        Me.Button17 = New System.Windows.Forms.Button()
-        Me.Button18 = New System.Windows.Forms.Button()
-        Me.Button19 = New System.Windows.Forms.Button()
-        Me.Button20 = New System.Windows.Forms.Button()
         Me.lstScores = New System.Windows.Forms.ListBox()
         Me.btnRoll = New System.Windows.Forms.Button()
         Me.btnMenu = New System.Windows.Forms.Button()
@@ -51,42 +40,45 @@ Partial Class yahtzee
         Me.btnReset = New System.Windows.Forms.Button()
         Me.btnExit = New System.Windows.Forms.Button()
         Me.grpDice.SuspendLayout()
-        Me.GroupBox2.SuspendLayout()
         Me.grpMenu.SuspendLayout()
         Me.SuspendLayout()
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(12, 75)
+        Me.Button1.Location = New System.Drawing.Point(18, 115)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(50, 50)
+        Me.Button1.Size = New System.Drawing.Size(75, 77)
         Me.Button1.TabIndex = 0
         Me.Button1.Text = "(dice visual)"
         Me.Button1.UseVisualStyleBackColor = True
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(68, 75)
+        Me.Button2.Location = New System.Drawing.Point(102, 115)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(50, 50)
+        Me.Button2.Size = New System.Drawing.Size(75, 77)
         Me.Button2.TabIndex = 1
         Me.Button2.Text = "(dice visual)"
         Me.Button2.UseVisualStyleBackColor = True
         '
         'Button3
         '
-        Me.Button3.Location = New System.Drawing.Point(180, 75)
+        Me.Button3.Location = New System.Drawing.Point(270, 115)
+        Me.Button3.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(50, 50)
+        Me.Button3.Size = New System.Drawing.Size(75, 77)
         Me.Button3.TabIndex = 3
         Me.Button3.Text = "(dice visual)"
         Me.Button3.UseVisualStyleBackColor = True
         '
         'Button4
         '
-        Me.Button4.Location = New System.Drawing.Point(124, 75)
+        Me.Button4.Location = New System.Drawing.Point(186, 115)
+        Me.Button4.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.Button4.Name = "Button4"
-        Me.Button4.Size = New System.Drawing.Size(50, 50)
+        Me.Button4.Size = New System.Drawing.Size(75, 77)
         Me.Button4.TabIndex = 2
         Me.Button4.Text = "(dice visual)"
         Me.Button4.UseVisualStyleBackColor = True
@@ -94,9 +86,10 @@ Partial Class yahtzee
         '
         'Button5
         '
-        Me.Button5.Location = New System.Drawing.Point(236, 75)
+        Me.Button5.Location = New System.Drawing.Point(354, 115)
+        Me.Button5.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.Button5.Name = "Button5"
-        Me.Button5.Size = New System.Drawing.Size(50, 50)
+        Me.Button5.Size = New System.Drawing.Size(75, 77)
         Me.Button5.TabIndex = 4
         Me.Button5.Text = "(dice visual)"
         Me.Button5.UseVisualStyleBackColor = True
@@ -114,27 +107,31 @@ Partial Class yahtzee
         Me.grpDice.Controls.Add(Me.Button4)
         Me.grpDice.Controls.Add(Me.Button2)
         Me.grpDice.Controls.Add(Me.Button1)
-        Me.grpDice.Location = New System.Drawing.Point(230, 150)
+        Me.grpDice.Location = New System.Drawing.Point(345, 231)
+        Me.grpDice.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.grpDice.Name = "grpDice"
-        Me.grpDice.Size = New System.Drawing.Size(300, 150)
+        Me.grpDice.Padding = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.grpDice.Size = New System.Drawing.Size(450, 231)
         Me.grpDice.TabIndex = 5
         Me.grpDice.TabStop = False
         Me.grpDice.Text = "Dice"
         '
         'Button6
         '
-        Me.Button6.Location = New System.Drawing.Point(236, 19)
+        Me.Button6.Location = New System.Drawing.Point(354, 29)
+        Me.Button6.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.Button6.Name = "Button6"
-        Me.Button6.Size = New System.Drawing.Size(50, 50)
+        Me.Button6.Size = New System.Drawing.Size(75, 77)
         Me.Button6.TabIndex = 9
         Me.Button6.Text = "(dice visual)"
         Me.Button6.UseVisualStyleBackColor = True
         '
         'Button7
         '
-        Me.Button7.Location = New System.Drawing.Point(180, 19)
+        Me.Button7.Location = New System.Drawing.Point(270, 29)
+        Me.Button7.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.Button7.Name = "Button7"
-        Me.Button7.Size = New System.Drawing.Size(50, 50)
+        Me.Button7.Size = New System.Drawing.Size(75, 77)
         Me.Button7.TabIndex = 8
         Me.Button7.Text = "(dice visual)"
         Me.Button7.UseVisualStyleBackColor = True
@@ -142,18 +139,20 @@ Partial Class yahtzee
         '
         'Button8
         '
-        Me.Button8.Location = New System.Drawing.Point(124, 19)
+        Me.Button8.Location = New System.Drawing.Point(186, 29)
+        Me.Button8.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.Button8.Name = "Button8"
-        Me.Button8.Size = New System.Drawing.Size(50, 50)
+        Me.Button8.Size = New System.Drawing.Size(75, 77)
         Me.Button8.TabIndex = 7
         Me.Button8.Text = "(dice visual)"
         Me.Button8.UseVisualStyleBackColor = True
         '
         'Button9
         '
-        Me.Button9.Location = New System.Drawing.Point(68, 19)
+        Me.Button9.Location = New System.Drawing.Point(102, 29)
+        Me.Button9.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.Button9.Name = "Button9"
-        Me.Button9.Size = New System.Drawing.Size(50, 50)
+        Me.Button9.Size = New System.Drawing.Size(75, 77)
         Me.Button9.TabIndex = 6
         Me.Button9.Text = "(dice visual)"
         Me.Button9.UseVisualStyleBackColor = True
@@ -161,162 +160,42 @@ Partial Class yahtzee
         '
         'Button10
         '
-        Me.Button10.Location = New System.Drawing.Point(12, 19)
+        Me.Button10.Location = New System.Drawing.Point(18, 29)
+        Me.Button10.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.Button10.Name = "Button10"
-        Me.Button10.Size = New System.Drawing.Size(50, 50)
+        Me.Button10.Size = New System.Drawing.Size(75, 77)
         Me.Button10.TabIndex = 5
         Me.Button10.Text = "(dice visual)"
         Me.Button10.UseVisualStyleBackColor = True
         Me.Button10.Visible = False
         '
-        'GroupBox2
-        '
-        Me.GroupBox2.Controls.Add(Me.Button11)
-        Me.GroupBox2.Controls.Add(Me.Button12)
-        Me.GroupBox2.Controls.Add(Me.Button13)
-        Me.GroupBox2.Controls.Add(Me.Button14)
-        Me.GroupBox2.Controls.Add(Me.Button15)
-        Me.GroupBox2.Controls.Add(Me.Button16)
-        Me.GroupBox2.Controls.Add(Me.Button17)
-        Me.GroupBox2.Controls.Add(Me.Button18)
-        Me.GroupBox2.Controls.Add(Me.Button19)
-        Me.GroupBox2.Controls.Add(Me.Button20)
-        Me.GroupBox2.Location = New System.Drawing.Point(347, 300)
-        Me.GroupBox2.Name = "GroupBox2"
-        Me.GroupBox2.Size = New System.Drawing.Size(300, 150)
-        Me.GroupBox2.TabIndex = 10
-        Me.GroupBox2.TabStop = False
-        Me.GroupBox2.Text = "AI Dice"
-        Me.GroupBox2.Visible = False
-        '
-        'Button11
-        '
-        Me.Button11.Enabled = False
-        Me.Button11.Location = New System.Drawing.Point(236, 19)
-        Me.Button11.Name = "Button11"
-        Me.Button11.Size = New System.Drawing.Size(50, 50)
-        Me.Button11.TabIndex = 9
-        Me.Button11.Text = "(dice visual)"
-        Me.Button11.UseVisualStyleBackColor = True
-        '
-        'Button12
-        '
-        Me.Button12.Enabled = False
-        Me.Button12.Location = New System.Drawing.Point(180, 19)
-        Me.Button12.Name = "Button12"
-        Me.Button12.Size = New System.Drawing.Size(50, 50)
-        Me.Button12.TabIndex = 8
-        Me.Button12.Text = "(dice visual)"
-        Me.Button12.UseVisualStyleBackColor = True
-        '
-        'Button13
-        '
-        Me.Button13.Enabled = False
-        Me.Button13.Location = New System.Drawing.Point(124, 19)
-        Me.Button13.Name = "Button13"
-        Me.Button13.Size = New System.Drawing.Size(50, 50)
-        Me.Button13.TabIndex = 7
-        Me.Button13.Text = "(dice visual)"
-        Me.Button13.UseVisualStyleBackColor = True
-        '
-        'Button14
-        '
-        Me.Button14.Enabled = False
-        Me.Button14.Location = New System.Drawing.Point(68, 19)
-        Me.Button14.Name = "Button14"
-        Me.Button14.Size = New System.Drawing.Size(50, 50)
-        Me.Button14.TabIndex = 6
-        Me.Button14.Text = "(dice visual)"
-        Me.Button14.UseVisualStyleBackColor = True
-        '
-        'Button15
-        '
-        Me.Button15.Enabled = False
-        Me.Button15.Location = New System.Drawing.Point(12, 19)
-        Me.Button15.Name = "Button15"
-        Me.Button15.Size = New System.Drawing.Size(50, 50)
-        Me.Button15.TabIndex = 5
-        Me.Button15.Text = "(dice visual)"
-        Me.Button15.UseVisualStyleBackColor = True
-        '
-        'Button16
-        '
-        Me.Button16.Enabled = False
-        Me.Button16.Location = New System.Drawing.Point(236, 75)
-        Me.Button16.Name = "Button16"
-        Me.Button16.Size = New System.Drawing.Size(50, 50)
-        Me.Button16.TabIndex = 4
-        Me.Button16.Text = "(dice visual)"
-        Me.Button16.UseVisualStyleBackColor = True
-        Me.Button16.Visible = False
-        '
-        'Button17
-        '
-        Me.Button17.Enabled = False
-        Me.Button17.Location = New System.Drawing.Point(180, 75)
-        Me.Button17.Name = "Button17"
-        Me.Button17.Size = New System.Drawing.Size(50, 50)
-        Me.Button17.TabIndex = 3
-        Me.Button17.Text = "(dice visual)"
-        Me.Button17.UseVisualStyleBackColor = True
-        Me.Button17.Visible = False
-        '
-        'Button18
-        '
-        Me.Button18.Enabled = False
-        Me.Button18.Location = New System.Drawing.Point(124, 75)
-        Me.Button18.Name = "Button18"
-        Me.Button18.Size = New System.Drawing.Size(50, 50)
-        Me.Button18.TabIndex = 2
-        Me.Button18.Text = "(dice visual)"
-        Me.Button18.UseVisualStyleBackColor = True
-        Me.Button18.Visible = False
-        '
-        'Button19
-        '
-        Me.Button19.Enabled = False
-        Me.Button19.Location = New System.Drawing.Point(68, 75)
-        Me.Button19.Name = "Button19"
-        Me.Button19.Size = New System.Drawing.Size(50, 50)
-        Me.Button19.TabIndex = 1
-        Me.Button19.Text = "(dice visual)"
-        Me.Button19.UseVisualStyleBackColor = True
-        Me.Button19.Visible = False
-        '
-        'Button20
-        '
-        Me.Button20.Enabled = False
-        Me.Button20.Location = New System.Drawing.Point(12, 75)
-        Me.Button20.Name = "Button20"
-        Me.Button20.Size = New System.Drawing.Size(50, 50)
-        Me.Button20.TabIndex = 0
-        Me.Button20.Text = "(dice visual)"
-        Me.Button20.UseVisualStyleBackColor = True
-        Me.Button20.Visible = False
-        '
         'lstScores
         '
         Me.lstScores.FormattingEnabled = True
-        Me.lstScores.Location = New System.Drawing.Point(34, 58)
+        Me.lstScores.ItemHeight = 20
+        Me.lstScores.Location = New System.Drawing.Point(51, 89)
+        Me.lstScores.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.lstScores.MultiColumn = True
         Me.lstScores.Name = "lstScores"
-        Me.lstScores.Size = New System.Drawing.Size(173, 303)
+        Me.lstScores.Size = New System.Drawing.Size(258, 464)
         Me.lstScores.TabIndex = 11
         '
         'btnRoll
         '
-        Me.btnRoll.Location = New System.Drawing.Point(455, 121)
+        Me.btnRoll.Location = New System.Drawing.Point(682, 186)
+        Me.btnRoll.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.btnRoll.Name = "btnRoll"
-        Me.btnRoll.Size = New System.Drawing.Size(75, 23)
+        Me.btnRoll.Size = New System.Drawing.Size(112, 35)
         Me.btnRoll.TabIndex = 12
         Me.btnRoll.Text = "Roll!"
         Me.btnRoll.UseVisualStyleBackColor = True
         '
         'btnMenu
         '
-        Me.btnMenu.Location = New System.Drawing.Point(230, 121)
+        Me.btnMenu.Location = New System.Drawing.Point(345, 186)
+        Me.btnMenu.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.btnMenu.Name = "btnMenu"
-        Me.btnMenu.Size = New System.Drawing.Size(75, 23)
+        Me.btnMenu.Size = New System.Drawing.Size(112, 35)
         Me.btnMenu.TabIndex = 13
         Me.btnMenu.Text = "Menu"
         Me.btnMenu.UseVisualStyleBackColor = True
@@ -325,46 +204,49 @@ Partial Class yahtzee
         '
         Me.grpMenu.Controls.Add(Me.btnReset)
         Me.grpMenu.Controls.Add(Me.btnExit)
-        Me.grpMenu.Location = New System.Drawing.Point(260, 300)
+        Me.grpMenu.Location = New System.Drawing.Point(390, 462)
+        Me.grpMenu.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.grpMenu.Name = "grpMenu"
-        Me.grpMenu.Size = New System.Drawing.Size(88, 150)
+        Me.grpMenu.Padding = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.grpMenu.Size = New System.Drawing.Size(132, 231)
         Me.grpMenu.TabIndex = 14
         Me.grpMenu.TabStop = False
         Me.grpMenu.Text = "Menu"
         '
         'btnReset
         '
-        Me.btnReset.Location = New System.Drawing.Point(6, 48)
+        Me.btnReset.Location = New System.Drawing.Point(9, 74)
+        Me.btnReset.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.btnReset.Name = "btnReset"
-        Me.btnReset.Size = New System.Drawing.Size(75, 23)
+        Me.btnReset.Size = New System.Drawing.Size(112, 35)
         Me.btnReset.TabIndex = 1
         Me.btnReset.Text = "Reset Game"
         Me.btnReset.UseVisualStyleBackColor = True
         '
         'btnExit
         '
-        Me.btnExit.Location = New System.Drawing.Point(6, 19)
+        Me.btnExit.Location = New System.Drawing.Point(9, 29)
+        Me.btnExit.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.btnExit.Name = "btnExit"
-        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.Size = New System.Drawing.Size(112, 35)
         Me.btnExit.TabIndex = 0
         Me.btnExit.Text = "Exit"
         Me.btnExit.UseVisualStyleBackColor = True
         '
         'yahtzee
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(1200, 692)
         Me.Controls.Add(Me.grpMenu)
         Me.Controls.Add(Me.btnMenu)
         Me.Controls.Add(Me.btnRoll)
         Me.Controls.Add(Me.lstScores)
-        Me.Controls.Add(Me.GroupBox2)
         Me.Controls.Add(Me.grpDice)
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.Name = "yahtzee"
         Me.Text = "Yahtzee"
         Me.grpDice.ResumeLayout(False)
-        Me.GroupBox2.ResumeLayout(False)
         Me.grpMenu.ResumeLayout(False)
         Me.ResumeLayout(False)
 
@@ -381,17 +263,6 @@ Partial Class yahtzee
     Friend WithEvents Button8 As Button
     Friend WithEvents Button9 As Button
     Friend WithEvents Button10 As Button
-    Friend WithEvents GroupBox2 As GroupBox
-    Friend WithEvents Button11 As Button
-    Friend WithEvents Button12 As Button
-    Friend WithEvents Button13 As Button
-    Friend WithEvents Button14 As Button
-    Friend WithEvents Button15 As Button
-    Friend WithEvents Button16 As Button
-    Friend WithEvents Button17 As Button
-    Friend WithEvents Button18 As Button
-    Friend WithEvents Button19 As Button
-    Friend WithEvents Button20 As Button
     Friend WithEvents lstScores As ListBox
     Friend WithEvents btnRoll As Button
     Friend WithEvents btnMenu As Button
