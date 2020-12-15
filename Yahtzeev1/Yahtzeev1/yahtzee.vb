cb690f4c734c91c1ca1d12a53b1c10db99ba326e
Patrick Reynolds <pareyn@microsoft.com>
2020-12-14 17:39:03 -0800

    Finalized game - fixed visual bug with alignment of the values marked for 3 of kind or 4 of kind due to character limit.

--- Yahtzeev1/Yahtzeev1/yahtzee.vb
+++ Yahtzeev1/Yahtzeev1/yahtzee.vb
@@ -32,8 +32,8 @@ Public Class yahtzee
         lstScores.Items.Add("Sixes")
         lstScores.Items.Add("Sum")
         lstScores.Items.Add("Bonus")
-        lstScores.Items.Add("Three-Kind")
-        lstScores.Items.Add("Four-Kind")
+        lstScores.Items.Add("Three-Of-A-Kind")
+        lstScores.Items.Add("Four-Of-A-Kind")
         lstScores.Items.Add("House")
         lstScores.Items.Add("S Straight")
         lstScores.Items.Add("L Straight")
@@ -64,8 +64,8 @@ Public Class yahtzee
         lstScores.Items(6) = ("Sixes")
         lstScores.Items(7) = ("Sum")
         lstScores.Items(8) = ("Bonus")
-        lstScores.Items(9) = ("Three-Kind")
-        lstScores.Items(10) = ("Four-Kind")
+        lstScores.Items(9) = ("Three-Of-A-Kind")
+        lstScores.Items(10) = ("Four-Of-A-Kind")
         lstScores.Items(11) = ("House")
         lstScores.Items(12) = ("S Straight")
         lstScores.Items(13) = ("L Straight")
@@ -854,10 +854,10 @@ Public Class yahtzee
         Next
 
         If bln3OfKind = True Then
-            lstScores.Items(9) = "Three-Kind" & ControlChars.Tab & ControlChars.Tab & CStr(AddFaces(intDiceArray))
+            lstScores.Items(9) = "Three-Of-A-Kind" & ControlChars.Tab & CStr(AddFaces(intDiceArray))
 
         Else
-            lstScores.Items(9) = "Three-Kind" & ControlChars.Tab & ControlChars.Tab & "0"
+            lstScores.Items(9) = "Three-Of-A-Kind" & ControlChars.Tab & "0"
 
         End If
 
@@ -895,10 +895,10 @@ Public Class yahtzee
 
         If bln4OfKind = True Then
 
-            lstScores.Items(10) = "Four-Kind" & ControlChars.Tab & ControlChars.Tab & CStr(AddFaces(intDiceArray))
+            lstScores.Items(10) = "Four-Of-A-Kind" & ControlChars.Tab & CStr(AddFaces(intDiceArray))
 
         Else
-            lstScores.Items(10) = "Four-Kind" & ControlChars.Tab & ControlChars.Tab & "0"
+            lstScores.Items(10) = "Four-Of-A-Kind" & ControlChars.Tab & "0"
 
         End If
 
